e0ab768ca16aa10775f12cee9fa755ae3fd99d20
wakame-se <jonybeans18@gmail.com>
2021-08-15 17:36:37 +0900

    マイグレーションファイルの修正

--- db/migrate/20210812090844_create_posts.rb
+++ db/migrate/20210812090844_create_posts.rb
@@ -3,7 +3,12 @@ class CreatePosts < ActiveRecord::Migration[6.0]
     create_table :posts do |t|
       t.text       :map_link, null: false
       t.integer    :distance, null: false
-      t.text       :text,     null: false
+      t.string     :course,   null: false
+      t.string     :slope,    null: false
+      t.string     :traffic,  null: false
+      t.string     :crowd,    null: false
+      t.string     :view,     null: false
+      t.text       :comment,  null: false
       t.references :user,     null: false, foreign_key: true
       t.timestamps
     end
