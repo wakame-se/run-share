ba25f7f04cc7b78181934bb16943f85301d30a75
wakame-se <jonybeans18@gmail.com>
2021-08-19 00:42:31 +0900

    MVCの編集およびJSによるヘッダープルダウン

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,35 @@
 class UsersController < ApplicationController
+  before_action :user_find
+  before_action :authenticate_user!, except: :show
+  before_action :move_to_index, except: :show
+
   def show
-    user = User.find(params[:id])
-    @posts = user.posts
+    @posts = @user.posts
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      bypass_sign_in(@user)
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_find
+    @user = User.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to root_path if current_user.id != @user.id
+  end
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :last_name, :first_name, :last_name_reading, :first_name_reading, :birthday)
   end
 end
