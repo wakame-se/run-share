444bf792f6d8a8dd7805b7d9a5a1ee3d102f1bf8
wakame-se <jonybeans18@gmail.com>
2021-08-20 18:02:29 +0900

    MVCの設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
-  before_action :post_find, only: %i[edit update]
+  before_action :post_find, only: %i[edit update destroy]
   before_action :authenticate_user!, except: %i[index show]
-  before_action :move_to_index, except: %i[index show]
+  before_action :move_to_index, only: %i[edit update destroy]
 
   def index
     @posts = Post.includes(:user).order(created_at: :DESC)
@@ -28,6 +28,11 @@ class PostsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to user_path(current_user.id)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
-  resources :posts, only: %i[new create edit update]
+  resources :posts, except: :index
   resources :users, only: %i[show edit update]
 end
