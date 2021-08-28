727260b668ab649613d709389df06ab2446344a7
wakame-se <jonybeans18@gmail.com>
2021-08-28 23:46:09 +0900

    commentsテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,7 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   def liked_by?(post_id)
     likes.where(post_id: post_id).exists?
