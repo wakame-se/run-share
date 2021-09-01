4c883b71c157f1c3913209295694fc127e35ad8e
wakame-se <jonybeans18@gmail.com>
2021-09-01 18:46:25 +0900

    Addressモデルの作成とマイグレーションファイルの編集

--- app/models/post.rb
+++ app/models/post.rb
@@ -16,4 +16,5 @@ class Post < ApplicationRecord
   has_one_attached :image
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_one :address, dependent: :destroy
 end
