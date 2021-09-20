4da7b2791f004493f2db1e25901eee24f559bac2
wakame-se <jonybeans18@gmail.com>
2021-09-20 14:08:22 +0900

    Update puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -10,9 +10,7 @@ threads min_threads_count, max_threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-# =========ローカルと本番切り替え===========
-# port        ENV.fetch('PORT') { 3000 } # 本番環境ではコメントアウト
-# ======================================
+port        ENV.fetch('PORT') { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
